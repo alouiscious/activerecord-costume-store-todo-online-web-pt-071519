a375e41471e4a3d9ee476eb151b694363d92796e
Alouiscious Media <info@reneesoul.com>
2019-10-08 02:59:41 -0700

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,12 +1,12 @@
 # Create your costume_stores migration here
-class CostumeStores < ActiveRecord::migration
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
   def change
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
-      t.string :costumes
-      t.string :employees
-      t.boolean :in_biz
+      t.string :costume_inventory
+      t.string :num_of_employees
+      t.boolean :still_in_business
       t.datetime :opening_time
       t.datetime :closing_time
 
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,15 +1,16 @@
 # Create your haunted_houses migration here
-class CreateHauntedHouses
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
   def change
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
       t.string :theme
       t.float :price
-      t.boolean :family_frnd
+      t.boolean :family_friendly
       t.datetime :opening_date
-      t.datetime :closing_closing
-      t.text :long_description
+      t.datetime :closing_date
+      t.text :description
 
     end
+  end
 end
