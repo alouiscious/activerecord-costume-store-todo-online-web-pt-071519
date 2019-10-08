38ad9f0b2b43561f66b7e8b75d412d9d1050abd5
Alouiscious Media <info@reneesoul.com>
2019-10-08 02:38:05 -0700

    git even after first rollbacks and learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+class CostumeStores < ActiveRecord::migration
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.string :costumes
+      t.string :employees
+      t.boolean :in_biz
+      t.datetime :opening_time
+      t.datetime :closing_time
+
+    end
+  end
+end
